ef38901b05bc68f2b479ba884b2f24d7e158dcd9
Zokirjon Mamadjonov <zikrcode@gmail.com>
2025-01-06 16:19:31 +0100

    upgrade min sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.zikrcode.thatword"
-        minSdk = 29
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
